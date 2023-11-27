025605c33fbdcd8ad531caaa4a0e1b4374fce1c2
emguse <30695541+emguse@users.noreply.github.com>
2023-11-27 22:02:21 +0900

    add unit test

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -1,8 +1,11 @@
+import datetime
+
 from pydantic import BaseModel, Field
 
 
 class TaskBase(BaseModel):
     title: str | None = Field(None, example="Go pick up the finished laundry")
+    due_date: datetime.date | None = Field(None, example="2025-12-01")
 
 
 class TaskCreate(TaskBase):
